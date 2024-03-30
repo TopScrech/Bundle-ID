d04aa913dc6e2060cc6c0403dd7ddad7071403d3
TopScrech <sergei_saliukov@icloud.com>
2024-03-30 02:13:40 +0100

    Initial Commit

---
+++ Bundle ID/Bundle_IDApp.swift
@@ -0,0 +1,32 @@
+//
+//  Bundle_IDApp.swift
+//  Bundle ID
+//
+//  Created by Sergei Saliukov on 30/03/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Bundle_IDApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
