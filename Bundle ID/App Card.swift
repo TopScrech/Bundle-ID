c83036fb08b54eada6b420da7242b1002d2404e9
TopScrech <sergei_saliukov@icloud.com>
2025-05-22 09:17:05 +0200

    Fetching & caching app img with Kingfisher

--- Bundle ID/App Card.swift
+++ Bundle ID/App Card.swift
@@ -1,4 +1,5 @@
-import SwiftUI
+import ScrechKit
+import Kingfisher
 
 struct AppCard: View {
     private let app: Result
@@ -10,22 +11,16 @@ struct AppCard: View {
     @State private var uiImage: UIImage?
     @State private var skOverlay = false
     
-    private let cache = NSCache<NSURL, UIImage>()
-    
     var body: some View {
         Button {
             skOverlay = true
         } label: {
             HStack {
-                if let uiImage {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .frame(width: 64, height: 64)
-                        .clipShape(.rect(cornerRadius: 16))
-                } else {
-                    ProgressView()
-                        .frame(width: 64, height: 64)
-                }
+                KFImage(URL(string: app.artworkUrl512))
+                    .resizable()
+                    .fade(duration: 0.25)
+                    .frame(width: 64, height: 64)
+                    .clipShape(.rect(cornerRadius: 16))
                 
                 VStack(alignment: .leading) {
                     Text(app.artistName)
@@ -71,38 +66,6 @@ struct AppCard: View {
             }
             
             ShareLink(item: app.trackViewUrl)
-        }
-        .task {
-            // cache.name = app.bundleID
-            guard let nsUrl = NSURL(string: app.bundleID) else {
-                return
-            }
-            
-            if let image = cache.object(forKey: nsUrl) {
-                uiImage = image
-            } else {
-                if let url = URL(string: app.artworkUrl512) {
-                    URLSession.shared.dataTask(with: url) { data, response, error in
-                        guard let data, error == nil else {
-                            return
-                        }
-                        
-                        DispatchQueue.main.async {
-                            let image = UIImage(data: data)
-                            
-                            if let image {
-                                cache.setObject(image, forKey: nsUrl)
-                                uiImage = image
-                            } else {
-                                print("Can't be converted to an image")
-                            }
-                        }
-                    }
-                    .resume()
-                } else {
-                    print("Invalid URL")
-                }
-            }
         }
     }
 }
